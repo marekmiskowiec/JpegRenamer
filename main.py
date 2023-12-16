0f7ee70e9c9d0f8300445828af983d4facfe2c42
Marek Miskowiec <lexiwdev@gmail.com>
2023-12-16 12:57:50 +0100

    Add error handling

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import os
 folder_path = "images"
 file_extension = "jpeg"
 
+# Check if the folder exists
+if not os.path.exists(folder_path):
+    print(f"The specified folder '{folder_path}' does not exist.")
+    exit()
+
 # Get the list of files in the specified folder
 file_list = [file for file in os.listdir(folder_path) if file.endswith(f".{file_extension}")]
+
+# Check if there are files to rename
+if not file_list:
+    print(f"No files with the extension '.{file_extension}' found in the folder.")
+    exit()
 
 # Sort the list of files based on numeric values in the file names
 sorted_file_list = sorted(file_list, key=lambda x: int(x.split(".")[0]))
@@ -20,6 +30,9 @@ for index, old_filename in enumerate(sorted_file_list, start=1):
     old_filepath = os.path.join(folder_path, old_filename)
     new_filepath = os.path.join(folder_path, new_filename)
 
-    # Rename the file and print the changes
-    os.rename(old_filepath, new_filepath)
-    print(f"{old_filename} ----> {new_filename}")
+    try:
+        # Rename the file and print the changes
+        os.rename(old_filepath, new_filepath)
+        print(f"{old_filename} ----> {new_filename}")
+    except Exception as e:
+        print(f"Error renaming file '{old_filename}': {str(e)}")
